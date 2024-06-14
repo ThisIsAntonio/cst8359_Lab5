2820816b67d0aab42e11431547d74109c9f687d4
Marcos Astudillo <antonio.ac86@hotmail.com>
2024-06-14 16:43:48 -0400

    Configure SportsDbContext and setup database connection

---
+++ Lab5/Data/DbInitializer.cs
@@ -0,0 +1,10 @@
+﻿namespace Lab5.Data
+{
+    public class DbInitializer
+    {
+        public static void Initialize(SportsDbContext context)
+        {
+            context.Database.EnsureCreated();
+        }
+    }
+}
