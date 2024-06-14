9b4b4588f98715cacf75709e69d0efa0dd29c2d2
Marcos Astudillo <antonio.ac86@hotmail.com>
2024-06-14 18:20:13 -0400

    Update SportClubsController and Index view to use NewsViewModel

--- Lab5/Controllers/HomeController.cs
+++ Lab5/Controllers/HomeController.cs
@@ -6,7 +6,16 @@ namespace Lab5.Controllers
     {
         public IActionResult Index()
         {
+            ViewData["Title"] = "Welcome";
+            ViewData["Message"] = "Welcome to the Lab5 application.";
+            ViewData["Comment"] = "My name is Marcos Astudillo \nAnd feel free to leave any comments or feedback about the assignments or course.";
+            return View();
+        }
+
+        public IActionResult Error()
+        {
             return View();
         }
     }
 }
+
